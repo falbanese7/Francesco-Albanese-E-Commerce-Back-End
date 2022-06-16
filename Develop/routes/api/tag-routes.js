219f43d70b834563f48079a8cd754abc6b545832
Francesco Albanese <falbanese96@gmail.com>
2022-06-16 00:29:10 -0400

    Added get and delete routres to product, category, and tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,33 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }, { model: ProductTag}]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk({
+      include: [{ model: Product }, { model: ProductTag}]
+    });
+
+    if (!tagData) {
+      res.status(404).json({message: 'No tag found with that id!'})
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
@@ -23,6 +45,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({message: 'No tag found with that id!'})
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch {err} {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
