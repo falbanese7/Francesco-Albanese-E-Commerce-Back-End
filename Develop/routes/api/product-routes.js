0501e5d61df243b26321d0db52037652616596bb
Francesco Albanese <falbanese96@gmail.com>
2022-06-17 04:24:35 -0400

    file revision

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -109,7 +109,7 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
-})
+});
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
